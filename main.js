19212f52a0ab64650bda907f16964bea55d661d6
galvanizejs <js@matthewleach.com>
2016-04-26 19:30:05 -0600

    Update main.js

--- main.js
+++ main.js
@@ -24,15 +24,13 @@ console.log("Question 1");
 
 
 
-
-
 // 2. Add an additional item to the beginning of your array.
 
 // 3. Remove the second and third items in your array.
 
 // 4. Add two new items after the second item.
 
-// 5. Write 'The current length of the array is....' using the .length method
+// 5. Write 'The current length of the array is...' using the .length method
 
 // Use the following Array for questions 6-11:
 var things = ['mug', 'book', 'mouse', 'plant', 'sunglasses'];
@@ -40,7 +38,8 @@ var things = ['mug', 'book', 'mouse', 'plant', 'sunglasses'];
 // 6. Change 'mouse' to 'keyboard'
 
 // 7. Combine all of the elements of the array into a string.
-//    (Hint: check out the 'join' method.)
+//    Apply a separator that displays the string like this: 
+//    "mug, book, mouse, plant, sunglasses" - notice the single space between the comma and the item
 
 // 8. Declare a variable called lastItem using .pop() on the things array.
 //    Add two new items to lastItem, one at the beginning and one at the end.
@@ -68,25 +67,38 @@ var people = ['Bill', 'Ted', 'Emily', 'Andrea', 'Doug'];
 
 // 16. Arrange the items alphabetically. Store this Array as orderedPeople
 
+
+
 // 17. Create an array of arrays with the following three arrays:
 var array1 = ["Fido", "Spot", "Rex", "Sparky"]
 var array2 = ["Bulldog", "Lab", "Dalmation", "Beagle"]
 var array3 = ["White", "Black", "Spotted", "Tri-color"]
 
-// Goal:
+// Goal: 
 var array4 = [
                 ["Fido", "Spot", "Rex", "Sparky"],
                 ["Bulldog", "Lab", "Dalmation", "Beagle"],
                 ["White", "Black", "Spotted", "Tri-color"]
               ]
+// Note: The brackets and comma's will not be displayed in the console like that, but just all the names in that order. 
 
 // 18. Remove "Sparky" and "White" from the above array of arrays.
 
-// BONUS 1: Try to arrange the following items from smallest to largest:
+// 19. Use the sort() method to arrange the following items from smallest to largest:
 var sortingNumbers = [2, 5, 98, 55, 77, 300]
-// If it doesn't sort as expected, explain why.
+// If it doesn't reorder the numbers as expected, explain why.
 
-// BONUS 2: Transform array1 into array2 using as few lines of code as you can without directly changing the value of an item (ie array1[0] = item)
+// BONUS Challenge: Transform array1 into array2 using as few lines of code 
+// as you can without directly changing the value of an item (ie array1[0] = item)
+// Hopefully, these challenges can be seen more like games - have fun solving this, but 
+// don't get too frustrated if this isn't as easy as you would help. 
+// Hint: If you get it complete in under 10 lines of code, you did good.
+//       If you get it complete in 5 lines of code, you did great.
+//       If you get it complete in 4 lines of code, you did amazing.
+//       If you get it complete in 3 lines of code, you did perfect.
+
 var array1 = [2, 'dog', 34, 'Bill', 'plant', 'mug', 17];
 // Goal:
 var array2 = ['plant', 17, 2, 'Bill', 'dog'];
+
+
